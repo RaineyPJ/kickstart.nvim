4e5ce5bb7ea3a5579909289d729e43ddc9402039
Patrick <patrick@innovolts.com>
2024-09-19 12:29:12 +0100

    Add Fugitive

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 --[[
-
     If you don't know anything about Lua, I recommend taking some time to read through
     a guide. One possible example which will only take 10-15 minutes:
       - https://learnxinyminutes.com/docs/lua/
@@ -164,6 +163,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
